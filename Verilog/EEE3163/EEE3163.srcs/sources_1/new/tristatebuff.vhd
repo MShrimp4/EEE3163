931253ebb972b05e792ed95ddabd07caa486f835
박창우 <101454106+freackpark@users.noreply.github.com>
2022-05-13 21:41:52 +0900

    Create tristatebuff.vhd

---
+++ Verilog/EEE3163/EEE3163.srcs/sources_1/new/tristatebuff.vhd
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2022/05/12 01:46:33
+-- Design Name: 
+-- Module Name: mux - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mux is
+    generic (length : integer := 1);
+    Port (
+    Din0: in  STD_LOGIC_VECTOR (length-1 downto 0);
+    Dout: out  STD_LOGIC_VECTOR (length-1 downto 0);
+    en: in STD_LOGIC
+     );
+end mux;
+
+architecture Behavioral of mux is
+    signal mux_input : STD_LOGIC_VECTOR (length-1 downto 0) := (others=>'Z');
+begin
+       mux_input<=(ohters=>'Z') when sel='0' else
+                  Din1 when sel='1' else
+                  (others=>'0');
+       Dout<=mux_input;
+end Behavioral;
