023922a9c6001b69714c9e3672d445fe9b401bcd
MShrimp4 <mshrimp@sogang.ac.kr>
2022-05-24 23:22:05 +0900

    Save .jl

--- FFT_DATA/fft_valid.jl
+++ FFT_DATA/fft_valid.jl
@@ -1,6 +1,19 @@
 using FFTW
 
-data1 = map(x->parse(Int,x),readlines("/home/mshrimp/Sample_Input_1.dat"))
+data1 = map(x->parse(Int,x),readlines("Sample_Input_1.dat"))
 data1_fft = map(abs,fft(data1))
 data1_fft_cut = map(x->trunc(Int, x/64),data1_fft)
 data1_fft_cut_hstr = map(x->Base.string(x,base=16,pad=1),data1_fft_cut)
+
+data2 = map(x->parse(Int,x),readlines("Sample_Input_2.dat"))
+data2_fft = map(abs,fft(data2))
+data2_fft_cut = map(x->trunc(Int, x/64),data2_fft)
+data2_fft_cut_hstr = map(x->Base.string(x,base=16,pad=1),data2_fft_cut)
+
+open("FFT_1.txt","w") do io
+   println(io,join(data1_fft_cut_hstr, "\n"))
+end
+
+open("FFT_2.txt","w") do io
+   println(io,join(data2_fft_cut_hstr, "\n"))
+end
